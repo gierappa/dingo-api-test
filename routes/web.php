001c46ee76d5672d498c81ddfb660a5900c70994
Mateusz Gierach <mateusz.gierach@webcoders.pl>
2018-06-21 09:38:52 +0200

    codeception for rest api tests

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
+/** @var \Dingo\Api\Routing\Router $api */
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+
+    $api->post('pet', 'App\Http\Controllers\Api\PetController@store');
+    $api->put('pet', 'App\Http\Controllers\Api\PetController@update');
+    $api->get('pet/findByTags', 'App\Http\Controllers\Api\PetController@findByTags');
+    $api->get('pet/{petId}', 'App\Http\Controllers\Api\PetController@get');
+    $api->post('pet/{petId}', 'App\Http\Controllers\Api\PetController@updateByPetId');
+    $api->delete('pet/{petId}', 'App\Http\Controllers\Api\PetController@delete');
+    $api->post('pet/{petId}/uploadImage', 'App\Http\Controllers\Api\PetController@uploadImageForPet');
+
+    $api->post('store/order', 'App\Http\Controllers\Api\\StoreController@get');
+    $api->post('store/order/{orderId}', 'App\Http\Controllers\Api\\StoreController@get');
+    $api->delete('store/order/{orderId', 'App\Http\Controllers\Api\\StoreController@get');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
